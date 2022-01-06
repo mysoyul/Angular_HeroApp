bf5cc89fb570853aa56ffaa5dadc1a900898bd6e
mysoyul <vega2k_@naver.com>
2022-01-06 14:36:29 +0900

    heroes component add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  constructor() {
+    console.log('AppComponent Constructor Called...');
+  }
 }
