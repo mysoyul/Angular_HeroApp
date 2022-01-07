7cc56d4dda344935f068bb3ce7eba5c92cd9eb6a
mysoyul <vega2k_@naver.com>
2022-01-07 11:15:50 +0900

    dashboard component update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { HeroService } from '../common/hero.service';
+import { Hero } from '../common/hero';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = []
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+                    .subscribe(hero_list => this.heroes = hero_list.slice(1,5));
   }
 
 }
