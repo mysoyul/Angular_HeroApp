5674df36253f7d8b1ae4433ee750dbba875ec0af
mysoyul <vega2k_@naver.com>
2022-01-07 10:37:33 +0900

    router module add

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../common/hero';
 import { HeroService } from '../common/hero.service';
+import { MessageService } from '../common/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,7 +16,7 @@ export class HeroesComponent implements OnInit {
 
   today: number = Date.now();
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
     console.log('HeroesComponent Constructor called..');
     console.log('HeroService ' + heroService);
   }
@@ -32,6 +33,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 
